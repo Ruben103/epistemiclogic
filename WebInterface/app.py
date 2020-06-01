8352e654d258fc0cfe7ecc04e769f4d4d0b51e7e
diarmuidk <diarmuid100@gmail.com>
2020-06-01 15:04:07 +0100

    interface support

--- WebInterface/app.py
+++ WebInterface/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 import time
+from main import playGame
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,6 +13,7 @@ class HelloWorld(Resource):
 
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(playGame, '/start')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 import random as rd
+from flask_restful import Resource
+
 
 class Game():
 
@@ -207,6 +209,13 @@ class Player():
         else:
             return 1, 2
 
+
+
+class playGame(Resource):
+    game = Game(num_players=4)
+    game.current_round.controller(game.current_round.starting_player)
+
+    print("BUGSTOPPER")
 
 if __name__ == '__main__':
     game = Game(num_players=4)

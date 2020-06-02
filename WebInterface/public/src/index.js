7ae2aad1d8a308ed913d2bdeedf5d4abffbe3dc8
diarmuidk <diarmuid100@gmail.com>
2020-06-02 13:02:22 +0100

    Website Ready

--- WebInterface/public/src/index.js
+++ WebInterface/public/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Blog from './blog/Blog'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Blog />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Blog />,
+  document.getElementById('blog')
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ WebInterface/public/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/bundle'),
+        filename: 'index_bundle.js'
+    },
+    devServer: {
+        inline: true,
+        port: 8001
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        query: {
+                            presets: ["@babel/preset-env", "@babel/preset-react"],
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.md$/i,
+                use: 'raw-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    },
+    node: {
+        fs: "empty"
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}

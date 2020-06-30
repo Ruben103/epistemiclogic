267a1d74f2a4dac00ab525fcba3126e1a2ceec31
diarmuidk <diarmuid100@gmail.com>
2020-06-30 18:10:49 +0200

    save images to static

--- WebInterface/public/src/blog/Blog.js
+++ WebInterface/public/src/blog/Blog.js
@@ -11,11 +11,7 @@ import Header from './Header';
 import MainFeaturedPost from './MainFeaturedPost';
 import FeaturedPost from './FeaturedPost';
 import Main from './Main';
-import Sidebar from './Sidebar';
 import Footer from './Footer';
-import post1 from './blog-post.1.md';
-import post2 from './blog-post.2.md';
-import post3 from './blog-post.3.md';
 import introArticle from './introArticle.md';
 import strategiesArticle from './strategiesArticle.md';
 import experimentArticle from './experimentArticle.md';
@@ -61,7 +57,6 @@ const featuredPosts = [
   },
 ];
 
-const posts = [post1, post2, post3];
 
 
 
